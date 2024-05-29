300c59765df8b06ff21d6daf85961563327ff020
Rus1kk <sapuanovagulnazym@gmail.com>
2024-05-29 17:43:53 +0500

    1-7

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,54 +2,50 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
+	}
+
+	ts, err := template.ParseFiles("./ui/html/home.page.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
 	}
 
 	w.Write([]byte("Привет из Snippetbox"))
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
+	fmt.Fprintf(w, "Отображение определенной заметки с ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-
 	if r.Method != http.MethodPost {
-
 		w.Header().Set("Allow", http.MethodPost)
-
-		http.Error(w, "Метод запрещен!", 405)
-
+		http.Error(w, "Метод не дозволен", 405)
 		return
 	}
 
 	w.Write([]byte("Создание новой заметки..."))
-}
-
-func main() {
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }
